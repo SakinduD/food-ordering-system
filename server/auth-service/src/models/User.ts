e846304088e9db84d741b74dddbe1daa942a5c06
Methush Anjula <methushanjula01@gmail.com>
2025-03-28 17:29:56 +0530

    Update User.ts
    add role to user

--- server/auth-service/src/models/User.ts
+++ server/auth-service/src/models/User.ts
@@ -1,10 +1,14 @@
 import mongoose, { Schema, Document } from 'mongoose';
 
+// Define the role types
+export type UserRole = 'customer' | 'restaurantOwner' | 'deliveryAgent';
+
 // Define the interface for the User document
 export interface IUser extends Document {
   name: string;
   email: string;
   password: string;
+  role: UserRole;
   isAdmin: boolean;
   isVerified: boolean;
   otp: string | null;
@@ -17,6 +21,11 @@ const userSchema: Schema = new Schema(
     name: { type: String, required: true },
     email: { type: String, unique: true, required: true },
     password: { type: String, required: true },
+    role: { 
+      type: String, 
+      enum: ['customer', 'restaurantOwner', 'deliveryAgent'], 
+      required: true 
+    },
     isAdmin: { type: Boolean, default: false },
     isVerified: { type: Boolean, default: false },
     otp: { type: String, default: null },

766251a10393bbc51b06069f357790db1ebee736
IshanDilmith <168458597+IshanDilmith@users.noreply.github.com>
2025-03-30 15:08:45 +0530

    implement the payment gateway

---
+++ server/payment-service/src/controllers/paymentController.ts
@@ -0,0 +1,98 @@
+import md5 from "crypto-js/md5";
+require('dotenv').config();
+import { Request, Response } from 'express';
+
+interface IOrderRequest extends Request {
+    user: {
+        userId: string;
+        role: string;
+    };
+}
+
+interface PaymentData {
+    merchantId: string;
+    return_url: string;
+    cancel_url: string;
+    notify_url: string;
+    merchantSecret: string;
+    first_name: string;
+    last_name: string;
+    email: string;
+    phone: string;
+    address: string;
+    city: string;
+    country: string;
+    orderId: string;
+    items: string;
+    currency: string;
+    amount: number;
+    userId: string;
+    hash?: string;
+}
+
+let paymentData: PaymentData | null = null;
+
+function generateHash(data: PaymentData): string {
+    const { merchantId, orderId, amount, currency, merchantSecret } = data;
+    const hashedSecret = md5(merchantSecret).toString().toUpperCase();
+    const amountFormatted = Number(amount).toFixed(2);
+    const hash = md5(merchantId + orderId + amountFormatted + currency + hashedSecret).toString().toUpperCase();
+    return hash;
+}
+
+// Create a payment
+const createPayment = (req: Request, res: Response) => {
+    const {
+        amount,
+        first_name,
+        last_name,
+        email,
+        phone,
+        address,
+        city,
+        currency = 'LKR',
+        orderId
+    } = req.body;
+
+    const { userId } = (req as IOrderRequest).user;
+
+    paymentData = {
+        merchantId: process.env.MERCHANT_ID as string || '',
+        return_url: 'http://localhost:5173/',
+        cancel_url: 'http://localhost:5173/',
+        notify_url: 'http://localhost:5173/',
+        merchantSecret: process.env.MERCHANT_SECRET as string || '',
+        first_name,
+        last_name,
+        email,
+        phone,
+        address,
+        city,
+        country: 'Sri Lanka',
+        orderId,
+        items: 'Wallet Top-up',
+        currency,
+        amount,
+        userId,
+    };
+
+    const hash = generateHash(paymentData);
+    paymentData.hash = hash;
+
+    res.json('paymet success for orderID: ' + paymentData.orderId); 
+};
+
+// Get the payment data
+const getPaymentData = (req: Request, res: Response): void => {
+    if (!paymentData) {
+        res.status(404).json({ message: 'No payment data found. Please create a payment first.' });
+        return;
+    }
+    res.json(paymentData);
+    return;
+};
+
+export default {
+    createPayment,
+    getPaymentData,
+};

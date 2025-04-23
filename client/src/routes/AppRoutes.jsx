bac199ca90ee364f908db110761b8e0438825e5f
Methush Anjula <methushanjula01@gmail.com>
2025-04-24 00:09:12 +0530

    Update AppRoutes.jsx

--- client/src/routes/AppRoutes.jsx
+++ client/src/routes/AppRoutes.jsx
@@ -6,17 +6,22 @@ import AdminMenuList from '../pages/restaurant-service/AdminMenuList';
 import CustomerMenuList from '../pages/restaurant-service/CustomerMenuList';
 import UserOrderList from '../components/Orders/userOrderList';
 import Login from '../pages/login/login';
+import Register from '../pages/register/Register';
 import RestaurantProfile from '../pages/restaurant-service/RestaurantProfile';
 import EditRestaurant from '../pages/restaurant-service/EditRestaurant';
 import DeliveryDriverAssignment from '../components/Delivery/DeliveryDriverAssignment';
 import CreateDelivery from  '../components/Delivery/CreateDelivery';
 import Cart from '../components/Orders/cart';
 
+import LandingPage from '../pages/landing-page/LandingPage';
+
+
+
 function AppRoutes() {
     return (
         <Router>
             <Routes>
-                <Route path="/" element={<div>Home</div>} />
+                <Route path="/" element={<LandingPage />} />
                 <Route path="/add-menu" element={<AddMenuItem />} />
                 <Route path="/edit-menu/:id" element={<EditMenuItem />} />
                 <Route path="/register-restaurant" element={<RegisterRestaurant />} />
@@ -24,6 +29,7 @@ function AppRoutes() {
                 <Route path="/menu" element={<CustomerMenuList />} />
                 
                 <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
                 <Route path="/restaurant-profile" element={<RestaurantProfile />} />
                 <Route path="/edit-restaurant" element={<EditRestaurant />} />
                 <Route path="/assign-driver" element={<DeliveryDriverAssignment />} />

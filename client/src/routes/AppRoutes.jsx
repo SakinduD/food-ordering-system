8dbda2f1436cdaa4563b552a40403dabf097f5fc
Methush Anjula <methushanjula01@gmail.com>
2025-04-24 10:31:35 +0530

    Update AppRoutes.jsx

--- client/src/routes/AppRoutes.jsx
+++ client/src/routes/AppRoutes.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import AddMenuItem from '../pages/restaurant-service/AddMenuItem';
 import EditMenuItem from '../pages/restaurant-service/EditMenuItem';
 import RegisterRestaurant from '../pages/restaurant-service/RegisterRestaurant';
@@ -15,33 +15,26 @@ import Cart from '../components/Orders/cart';
 
 import LandingPage from '../pages/landing-page/LandingPage';
 
-
-
 function AppRoutes() {
     return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<LandingPage />} />
-                <Route path="/add-menu" element={<AddMenuItem />} />
-                <Route path="/edit-menu/:id" element={<EditMenuItem />} />
-                <Route path="/register-restaurant" element={<RegisterRestaurant />} />
-                <Route path="/admin/menu" element={<AdminMenuList />} />
-                <Route path="/menu" element={<CustomerMenuList />} />
-                
-                <Route path="/login" element={<Login />} />
-                <Route path="/register" element={<Register />} />
-                <Route path="/restaurant-profile" element={<RestaurantProfile />} />
-                <Route path="/edit-restaurant" element={<EditRestaurant />} />
-                <Route path="/assign-driver" element={<DeliveryDriverAssignment />} />
-                <Route path="/create-delivery" element={<CreateDelivery />} />
+        <Routes>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/add-menu" element={<AddMenuItem />} />
+            <Route path="/edit-menu/:id" element={<EditMenuItem />} />
+            <Route path="/register-restaurant" element={<RegisterRestaurant />} />
+            <Route path="/admin/menu" element={<AdminMenuList />} />
+            <Route path="/menu" element={<CustomerMenuList />} />
 
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/restaurant-profile" element={<RestaurantProfile />} />
+            <Route path="/edit-restaurant" element={<EditRestaurant />} />
+            <Route path="/assign-driver" element={<DeliveryDriverAssignment />} />
+            <Route path="/create-delivery" element={<CreateDelivery />} />
 
-
-
-                <Route path="/orders" element={<UserOrderList />} />
-                <Route path="/cart" element={<Cart />} />
-            </Routes>
-        </Router>
+            <Route path="/orders" element={<UserOrderList />} />
+            <Route path="/cart" element={<Cart />} />
+        </Routes>
     );
 }
 

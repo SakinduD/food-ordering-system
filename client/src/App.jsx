6e77c4008148a895692d625f75a23bfb2db040d4
Methush Anjula <methushanjula01@gmail.com>
2025-04-24 10:31:10 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,23 @@
 import './App.css';
+import { BrowserRouter as Router } from 'react-router-dom';
 import AppRoutes from './routes/AppRoutes';
 import UserContextProvider from './context/userContext';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <UserContextProvider>
-      <AppRoutes />
+      <Router>
+        <div className="min-h-screen flex flex-col">
+          <Header />
+          <main className="flex-grow">
+            <AppRoutes />
+          </main>
+          <Footer />
+        </div>
+      </Router>
     </UserContextProvider>
-    
   );
 }
 

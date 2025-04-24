105d181e2b1b490abb1bd700f449088ff69f676e
Methush Anjula <methushanjula01@gmail.com>
2025-04-24 10:31:25 +0530

    Update LandingPage.jsx

--- client/src/pages/landing-page/LandingPage.jsx
+++ client/src/pages/landing-page/LandingPage.jsx
@@ -1,4 +1,3 @@
-import Header from "./components/Header"
 import HeroSection from "./components/HeroSection"
 import FeaturesSection from "./components/FeaturesSection"
 import HowItWorksSection from "./components/HowItWorksSection"
@@ -6,12 +5,10 @@ import RestaurantsSection from "./components/RestaurantsSection"
 import TestimonialsSection from "./components/TestimonialsSection"
 import AppDownloadSection from "./components/AppDownloadSection"
 import CTASection from "./components/CTASection"
-import Footer from "./components/Footer"
+
 
 function LandingPage() {
   return (
-    <div className="flex min-h-screen flex-col">
-      <Header />
       <main className="flex-1">
         <HeroSection />
         <FeaturesSection />
@@ -21,8 +18,6 @@ function LandingPage() {
         <AppDownloadSection />
         <CTASection />
       </main>
-      <Footer />
-    </div>
   )
 }
 
